ecf9b3892443d96616ebe9fd70754a0d0c854431
edmarpurcina <edmar.purcina@gmail.com>
2020-11-09 22:01:57 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, session
 import dicionarios, time
 
 app = flask.Flask(__name__)
@@ -14,78 +14,74 @@ def home():
 
     return" <h1>Olá Mundo</h1>"
 
-@app.route('/api/v1/', methods=['GET'])
+@app.route('/api/v1/all', methods=['GET'])
 def api_all():
-    return jsonify(dictpessoas)
-
-def retorna_bens(cpf):
-    posses = []
-    for bem in dictbens:
-        if bem['proprietario'] == cpf:
-           posses.append(bem)
-
-    return posses
+    return jsonify(dictpessoas, dictempresas, dictbens)
 
 def retorna_pessoas(cpf):
     dadospessoa = []
     for pessoa in dictpessoas:
-        if pessoa['cpf'] == cpf:
+        if pessoa['cpf'] == cpf :
             dadospessoa = pessoa
     if len(dadospessoa) <= 0:
         dadospessoa = "Cpf nao Encontrado"
 
     return dadospessoa
 
-def lista_proprietarios(doc):
-    dadosproprietario = []
-    dicionario = {}
-    for documentos in doc:
-        if 'cpf' in documentos.keys():
-            dados = retorna_pessoas(documentos['cpf'])
-            dadosproprietario.append(dados)
-            dicionario.update({'proprietario':dadosproprietario})
-            print(dicionario)
+def lista_proprietarios(proprietarios, empresa):
+    pessoa = []
+    for proprietario in proprietarios:
+        if len(proprietario) is 11:
+            pessoa.append(retorna_pessoas(proprietario))
+            empresa.update({'proprietarios': pessoa})
+        elif len(proprietario) is 14:
+            pessoa.append(retorna_dados_empresa(proprietario))
+            empresa.update({'proprietarios': pessoa})
         else:
-            dados = retorna_dados_empresa(documentos['cnpj'])
-            dadosproprietario.append(dados)
-            dicionario.update({'proprietario': dadosproprietario})
-            print(dicionario)
+            break
 
-    return dadosproprietario
+    return empresa
 
 def retorna_dados_empresa(cnpj):
     dadosempresa = []
     for empresa in dictempresas:
         if empresa['cnpj'] == cnpj:
             proprietarios = empresa['proprietarios']
-            empresaproprietarios = lista_proprietarios(empresa)
+            dadosempresa = lista_proprietarios(proprietarios, empresa)
 
-            dadosempresa = empresa
-    #print(empresaproprietarios)
     return dadosempresa
 
+def retorna_bens(cpf):
+    posses = []
+    for bem in dictbens:
+        if bem['proprietario'] == cpf:
+           posses.append(bem)
+    #retorna nome e cnpj da empresa
+    for empresa in dictempresas:
+        if cpf in empresa['proprietarios']:
+            posses.append({'nome da empresa': empresa['nome da empresa'], 'cnpj': empresa['cnpj']})
+
+    return posses
 
 @app.route('/api/v1/busca', methods=['GET'])
 def api_busca():
-    results = []
     if 'cpf' in request.args:
         if len(request.args['cpf']) != 11:
             return "Error: CPF Invalido"
-
         cpf = request.args['cpf']
-        pessoa = retorna_pessoas(cpf)
-        return jsonify(pessoa)
-
+        results = retorna_pessoas(cpf)
     elif 'cnpj' in request.args:
         if len(request.args['cnpj']) != 14:
             return "Error: CNPJ Invalido"
-
         cnpj = request.args['cnpj']
-        dadosempresa = retorna_dados_empresa(cnpj)
-        return jsonify(dadosempresa)
-
+        results = retorna_dados_empresa(cnpj)
+    elif 'posses' in request.args:
+        if len(request.args['posses']) != 11:
+            return "Error: CPF Invalido"
+        busca = request.args['posses']
+        results = retorna_bens(busca)
     else:
-        return "Error: No CPF Provide"
+        return "Error: No Data Provide"
 
     return jsonify(results)
 
